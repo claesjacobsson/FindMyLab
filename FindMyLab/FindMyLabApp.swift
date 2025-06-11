94dac8daa511cb614c2d5ec8d477d031c729e928
Claes Jacobsson <claes.jacobsson@gmail.com>
2025-06-11 16:28:42 +0200

    Initial Commit

---
+++ FindMyLab/FindMyLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyLabApp.swift
+//  FindMyLab
+//
+//  Created by Claes Jacobsson on 2025-06-11.
+//
+
+import SwiftUI
+
+@main
+struct FindMyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
