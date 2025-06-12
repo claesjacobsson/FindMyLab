c9138e26051bdb843a0bdd2c13c3498c14d847bf
Claes Jacobsson <claes.jacobsson@gmail.com>
2025-06-12 12:02:37 +0200

    Rough reproduction of Apple’s FindMy UI

--- FindMyLab/ContentView.swift
+++ FindMyLab/ContentView.swift
@@ -6,19 +6,130 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map()
+            .sheet(isPresented: Binding(projectedValue: .constant(true))) {
+                CustomTabView()
+                    .presentationDetents([.height(80), .medium, .large])
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct CustomTabView: View {
+
+    @State private var selectedTab: CustomTab = .people
+
+    var body: some View {
+        VStack {
+            tabContent
+
+            // Custom expandable tab bar
+            HStack {
+                Group {
+                    ForEach(CustomTab.allCases, id: \.self) { tab in
+                        Button {
+                            selectedTab = tab
+                        } label: {
+                            VStack {
+                                Image(systemName: tab.icon)
+                                Text(tab.title).font(.footnote)
+                            }
+                        }
+                        .tint(selectedTab == tab ? .blue : .black)
+                    }
+                }
+                .font(.title2)
+                .frame(maxWidth: .infinity)
+            }
+            .frame(height: 80)
+        }
+    }
+
+    @ViewBuilder
+    var tabContent: some View {
+        switch selectedTab {
+            case .people:
+                NavigationView {
+                    List {
+                        ForEach(0..<20) {
+                            Text("Person \($0)")
+                        }
+                        .listRowBackground(Color.clear)
+                    }
+                    .listStyle(.plain)
+                    .navigationTitle("People")
+                }
+            case .devices:
+                NavigationView {
+                    List {
+                        ForEach(0..<20) {
+                            Text("Device \($0)")
+                        }
+                        .listRowBackground(Color.clear)
+                    }
+                    .listStyle(.plain)
+                    .navigationTitle("Devices")
+                }
+            case .items:
+                NavigationView {
+                    List {
+                        ForEach(0..<20) {
+                            Text("Item \($0)")
+                        }
+                        .listRowBackground(Color.clear)
+                    }
+                    .listStyle(.plain)
+                    .navigationTitle("Items")
+                }
+            case .me:
+                NavigationView {
+                    Image(systemName: "person.circle.fill")
+                        .font(.largeTitle)
+                    .navigationTitle("Me")
+                }
+        }
+    }
+
+}
+
+enum CustomTab: CaseIterable {
+    case people
+    case devices
+    case items
+    case me
+
+    var icon: String {
+        switch self {
+            case .people:
+                return "figure.2"
+            case .devices:
+                return "macbook.and.iphone"
+            case .items:
+                return "circle.grid.2x2.fill"
+            case .me:
+                return "location.slash"
+        }
+    }
+
+    var title: String {
+        switch self {
+            case .people:
+                return "People"
+            case .devices:
+                return "Devices"
+            case .items:
+                return "Items"
+            case .me:
+                return "Me"
+        }
+    }
 }
